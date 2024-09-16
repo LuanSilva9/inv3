8ae3b824fc656b39acb1a7b11d80404f3ab9fc46
Luan <luanpdev@gmail.com>
2024-09-16 15:41:17 +0000

    Atualizando mensagem de inicio

--- src/Constants/UserTimeResponse.js
+++ src/Constants/UserTimeResponse.js
@@ -1,7 +1,21 @@
 const date = new Date();
 
 export default function Time() {
-    const mensagem = date.getHours() > 18 ? "Boa Noite!" : "Bom Dia";
+    const hour = date.getHours();
+    let message;
 
-    return <h2>{mensagem}</h2>
+    if(hour >= 6 && hour < 12) {
+        message = "Bom Dia!";
+    }
+    else if (hour >= 12 && hour < 18) {
+        message = "Boa Tarde!";
+    } 
+    else if (hour >= 18 && hour < 24) {
+        message = "Boa Noite!";
+    }
+    else if (hour >= 0 && hour < 6) {
+        message = "Boa Madrugada!";
+    }
+
+    return <h2>{message}</h2>
 }
