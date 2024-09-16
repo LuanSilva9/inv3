d993d4942f8093736348361cdaa273bd16f61eff
Luan <luanpdev@gmail.com>
2024-09-16 20:33:16 +0000

    Mudando icones e cores das sessões para melhor vizualização

--- src/Constants/InvestType.js
+++ src/Constants/InvestType.js
@@ -1,39 +1,37 @@
 import { BsNvidia } from "react-icons/bs";
 import { FaAmazon, FaApple, FaBitcoin, FaEthereum, FaMicrosoft } from "react-icons/fa";
 import { GiAnimalHide } from "react-icons/gi";
+import { IoIosBusiness } from "react-icons/io";
 import { LuJapaneseYen } from "react-icons/lu";
-import { RiMoneyPoundCircleFill } from "react-icons/ri";
 import { SiPolygon } from "react-icons/si";
-
-const getRandomValue = () => (Math.random() * 1000).toFixed(2);
 
 const InvestType = [
     {
         name: "Ações",
-        icon: <RiMoneyPoundCircleFill />,
-        color: "#4a6d23",
+        icon: <IoIosBusiness />,
+        color: "#6264b3",
         investProps: [
             {
                 nameInvest: "Nvidia®",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#5a8a1d",
                 defaultIcon: <BsNvidia />
             },
             {
                 nameInvest: "Amazon®",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#e09b2b",
                 defaultIcon: <FaAmazon />
             },
             {
                 nameInvest: "Apple Inc.®",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#424242",
                 defaultIcon: <FaApple />
             },
             {
                 nameInvest: "Microsoft",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#0064a3",
                 defaultIcon: <FaMicrosoft />
             }
@@ -46,31 +44,31 @@ const InvestType = [
         investProps: [
             {
                 nameInvest: "Bitcoin",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#f2a40e",
                 defaultIcon: <FaBitcoin />
             },
             {
                 nameInvest: "Etherum",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#4c4c4c",
                 defaultIcon: <FaEthereum />
             },
             {
                 nameInvest: "HoneyBadgers",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#e37124",
                 defaultIcon: <GiAnimalHide />
             },
             {
                 nameInvest: "Satoshi",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#5a5a5a",
                 defaultIcon: <LuJapaneseYen />
             },
             {
                 nameInvest: "Matic",
-                currentValue: getRandomValue(),
+                currentValue: 0,
                 defaultColor: "#6c4da4",
                 defaultIcon: <SiPolygon />
             }
