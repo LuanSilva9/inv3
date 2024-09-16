fe1d24a9dbdeee17ac362cc59eae0aab4929dfca
Luan <luanpdev@gmail.com>
2024-09-16 20:34:02 +0000

    Criando formulario para alteração do valor atual investido / Setando valor inicial como 0

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,25 +1,57 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Card({ elementForInvest }) {
-    const [name, setName] = useState(elementForInvest.nameInvest.toLowerCase())
+    const [name, setName] = useState(elementForInvest.nameInvest.toLowerCase());
+    const [openForm, setOpenForm] = useState(false);
+    const [currentValue, setCurrentValue] = useState(elementForInvest.currentValue)
+
+    async function toggleChange() {
+        await setOpenForm(!openForm);
+    }
+
+    async function currentChange(event) {
+        event.preventDefault();
+        
+        elementForInvest.currentValue = ( (!currentValue || isNaN(currentValue) || currentValue < 0) ? 0 : currentValue );
+
+        toggleChange();
+    }
 
     return (
         <div className="card-single">
-            <div className="card-title" style={{color: elementForInvest.defaultColor}}>
+            <div className="card-title" style={{ textAlign: "center", color: elementForInvest.defaultColor }}>
                 <h3>{elementForInvest.nameInvest}</h3>
                 {elementForInvest.defaultIcon}
             </div>
-            <div className="current">
-                <p>Valor Investido: {elementForInvest.currentValue}U$</p>
-            </div>
+            {
+                openForm ? (
+                    <div className="current">
+                        <form className="current-form" onSubmit={currentChange}>
+                            <label htmlFor="investiments">Valor Investido: </label>
+                            <div className="form-control">
+                                <input type="text" name="investiments" id="investiments" onChange={(e) => setCurrentValue(e.target.value)} value={currentValue} />
+
+                                <button type="submit" className="alterar-btn" style={{ background: elementForInvest.defaultColor }}>
+                                    Alterar valor
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                ) : (
+                    <div className="current">
+                        <p>Valor Investido: {elementForInvest.currentValue}U$</p>
+                    </div>
+                )
+            }
+            
             <div className="options">
                 <a href={`/grafico/${name}`} className="grafico-btn">
                     Acompanhar Grafico
                 </a>
-                <a href="/atualizar-valor" className="atualizar-btn">
-                    Remover ou alterar valor
-                </a>
+                <button onClick={toggleChange} className="atualizar-btn">
+                    {openForm ? "Cancelar" : "Alterar valor"}
+                </button>
             </div>
         </div>
-    )
+    );
 }
