06e7eca5837d65d799aa93a0a4ddccf372b27e3f
Luan <luanpdev@gmail.com>
2024-09-16 20:46:53 +0000

    Mudando Inicialização, HeroSection e transições mais suaves

--- src/Components/Hero/HeroSection.jsx
+++ src/Components/Hero/HeroSection.jsx
@@ -3,8 +3,7 @@ import UserTimeResponse from "../../Constants/UserTimeResponse";
 export default function HeroSection() {
     return (
         <section className="hero-section">
-            <h2>Olá, Usuario(a)!</h2>
-            <UserTimeResponse/>
+            <h2>Olá, Usuario(a)<UserTimeResponse/></h2>
         </section>
     )
 }
